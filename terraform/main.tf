19dd5c2271a4721f436f02e8734a97458ccdfd52
Daniel Batista <danielbped@gmail.com>
2024-05-07 22:05:09 -0300

    feat: instances db with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,9 +3,27 @@ provider "google" {
   region  = var.region
 }
 
+resource "google_sql_database_instance" "mysql_instance" {
+  name             = "mysql-instance"
+  database_version = "MYSQL_5_7"
+  region           = var.region
+  project          = var.project_id
+
+  settings {
+    tier = "db-f1-micro"
+  }
+}
+
+resource "google_sql_database" "mysql_db" {
+  name     = "chatprt-db"
+  instance = google_sql_database_instance.mysql_instance.name
+}
+
 resource "google_cloud_run_service" "backend" {
   name     = "backend-service"
   location = var.region
+
+  depends_on = [google_sql_database.mysql_db]
 
   template {
     spec {
